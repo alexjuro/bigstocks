c9df6e374ec5307ff8f51573fbb86fb7fbc48c52
Nico Pareigis <nico@pareigis.de>
2023-04-27 13:19:34 +0200

    feat: 'loading' display while waiting for api-server response

--- client/src/components/user-profile/user-profile.ts
+++ client/src/components/user-profile/user-profile.ts
@@ -1,5 +1,6 @@
 import { html, LitElement } from 'lit';
 import { customElement, query } from 'lit/decorators.js';
+import { until } from 'lit/directives/until.js';
 import sharedStyle from '../shared.css?inline';
 import componentStyle from './user-profile.css?inline';
 import { httpClient } from '../../http-client';
@@ -16,70 +17,69 @@ class Profile extends PageMixin(LitElement) {
   static styles = [sharedStyle, componentStyle];
 
   @query('form') form!: HTMLFormElement;
-  @query('#Username') name!: HTMLInputElement;
-  @query('#Email') email!: HTMLInputElement;
-  @query('#Password') password!: HTMLInputElement;
+  @query('#name') name!: HTMLInputElement;
+  @query('#email') email!: HTMLInputElement;
+  @query('#password') password!: HTMLInputElement;
 
-  private user!: User;
+  private userInfo = httpClient.get('/users/profile');
 
   constructor() {
     super();
     // TODO: check auth header; redirect to sign-in if not present
   }
 
-  async connectedCallback() {
-    super.connectedCallback();
-
-    this.user = await httpClient
-      .get('/users/profile')
-      .then(res => res.json())
-      .catch(() => {
-        // TODO: pop-up 'failed to load'
-        // FIX: error in render, even though this should redirect immediately?
-        window.location.assign('/');
-      });
-  }
-
   render() {
     return html`
-      ${this.renderNotification()}
-      <h2>Profile</h2>
-      <div id="avatar">
-        <h3>Avatar</h3>
-        <p>profile_picture_here</p>
-      </div>
-      <div class="divider"><hr /></div>
-      <div id="settings">
-        <h3>Account Details</h3>
-        <form>
-          <div>
-            <label for="name">Username</label>
-            <input id="name" type="text" value=${this.user.name} required />
-            <span />
-            <div class="invalid-feedback">Username must not be empty.</div>
-            <div id="annotation">Your public username.</div>
-          </div>
-          <div>
-            <label for="email">Email address</label>
-            <input id="email" type="email" value=${this.user.email} required />
-            <span />
-            <div class="invalid-feedback">Email address must be a valid email.</div>
-            <div id="annotation">Your email address.</div>
-          </div>
-          <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" minlength="8" />
-            <span />
-            <div class="invalid-feedback">Password must not be longer than 7 characters.</div>
-            <div id="annotation">Your account password.</div>
-          </div>
-          <div class="divider"><hr /></div>
-          <div>
-            <button type="button" @click=${this.submit}>Save</button>
-            <button type="reset">Cancel</button>
-          </div>
-        </form>
-      </div>
+      ${until(
+        this.userInfo
+          .then(async res => {
+            const user: User = await res.json();
+
+            return html` ${this.renderNotification()}
+              <h2>Profile</h2>
+              <div id="avatar">
+                <h3>Avatar</h3>
+                <p>profile_picture_here</p>
+              </div>
+              <div class="divider"><hr /></div>
+              <div id="settings">
+                <h3>Account Details</h3>
+                <form novalidate>
+                  <div>
+                    <label for="name">Username</label>
+                    <input id="name" type="text" value=${user.name} required />
+                    <span />
+                    <div class="invalid-feedback">Username must not be empty.</div>
+                    <div id="annotation">Your public username.</div>
+                  </div>
+                  <div>
+                    <label for="email">Email address</label>
+                    <input id="email" type="email" value=${user.email} required />
+                    <span />
+                    <div id="annotation">Your email address.</div>
+                    <div class="invalid-feedback">Email address must be a valid email.</div>
+                  </div>
+                  <div>
+                    <label for="password">Password</label>
+                    <input id="password" type="password" minlength="8" />
+                    <span />
+                    <div id="annotation">Your account password.</div>
+                    <div class="invalid-feedback">Password must be longer than 7 characters.</div>
+                  </div>
+                  <div class="divider"><hr /></div>
+                  <div>
+                    <button type="button" @click=${this.submit}>Save</button>
+                    <button type="reset">Cancel</button>
+                  </div>
+                </form>
+              </div>`;
+          })
+          .catch(() => {
+            // TODO: pop-up 'failed to load'
+            window.location.assign('/');
+          }),
+        html`Loading...`
+      )}
     `;
   }
 
