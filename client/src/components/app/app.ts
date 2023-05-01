8212fbc9d69fc1f48a0994b19cbacbc37dcea044
Alexander <al568412@fh-muenster.de>
2023-05-01 16:29:02 +0200

    feat: app.ts now renders components

--- client/src/components/app/app.ts
+++ client/src/components/app/app.ts
@@ -1,13 +1,25 @@
 /* Autor: Alexander Lesnjak */
 
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import sharedStyle from '../shared.css?inline'
+import { customElement, state } from 'lit/decorators.js';
+import { httpClient } from '../../http-client';
+import { router } from '../../router/router';
 
 @customElement('app-root')
 class AppComponent extends LitElement {
+  @state() private services = [{ title: 'Leaderboard', routePath: 'leaderboard' }];
+
+  constructor() {
+    super();
+    const port = 3000;
+    httpClient.init({ baseURL: `${location.protocol}//${location.hostname}:${port}/api/` });
+  }
+
   render() {
-    const styles = sharedStyle;
-    return html`<h1>Hello World!</h1>`;
+    return html`
+    <app-header2></app-header2>
+    <app-leaderboard2></app-leaderboard2>`;
   }
 }
+
+
