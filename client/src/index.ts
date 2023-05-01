003415ca7e5fe8b9dae7db385b4d1f7adf3d93d9
Alexander <al568412@fh-muenster.de>
2023-05-01 16:20:45 +0200

    feat: created compnents

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,6 @@
 /* Autor: Prof. Dr. Norman Lahme-Hütig (FH Münster) */
 
 import './components/app/app.ts';
-import './components/header/header.ts';
-import './components/leaderboard/leaderboard.ts'
+import './components/header2/header2.ts';
+import './components/leaderboard2/leaderboard2.ts'
 
