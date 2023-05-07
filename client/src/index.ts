828cd308f7124dbea8d74b80523d8c2b32bea9ac
Alexander Lesnjak <al568412@fh-muenster.de>
2023-05-07 19:09:36 +0200

    refactor: basically everything

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,5 @@
 /* Autor: Prof. Dr. Norman Lahme-Hütig (FH Münster) */
 
 import './components/app/app.ts';
-import './components/header2/header2.ts';
-import './components/leaderboard2/leaderboard2.ts'
+import './components/header3/header.ts';
 
