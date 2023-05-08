ed8faa5e2d43e0583a9274f7961c9c8d53813fdc
Alexander Lesnjak <al568412@fh-muenster.de>
2023-05-08 21:57:42 +0200

    page in app.ts uebernommen

--- client/src/components/app/app.ts
+++ client/src/components/app/app.ts
@@ -3,11 +3,18 @@
 import { LitElement, html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { httpClient } from '../../http-client';
-import { router } from '../../router/router';
+import sharedStyle from '../shared.css?inline';
+import componentStyle from './app.css?inline';
 
 @customElement('app-root')
 class AppComponent extends LitElement {
-  @state() private services = [{ title: 'Leaderboard', routePath: 'leaderboard' }];
+  @state() private services = [{ title: 'bigstocks', routePath: '' }];
+
+  static styles = componentStyle;
+
+  pos = 100;
+  page = 1;
+  pages = ['news', 'leaderboard', 'protfolio', 'profile'];
 
   constructor() {
     super();
@@ -15,11 +22,80 @@ class AppComponent extends LitElement {
     httpClient.init({ baseURL: `${location.protocol}//${location.hostname}:${port}/api/` });
   }
 
+  /*
+    function left() {
+      if (pos == 0) {
+        maincontainer!.style.right = pos + '%';
+      } else {
+        pos = pos - 100;
+        maincontainer!.style.right = pos + '%';
+      }
+    }
+
+    function right() {
+      if (pos == 300) {
+        maincontainer!.style.right = pos + '%';
+      } else {
+        pos = pos + 100;
+        maincontainer!.style.right = pos + '%';
+      }
+    }
+
+    function news() {
+      maincontainer!.style.right = '0%';
+    }
+
+    function leaderboard() {
+      maincontainer!.style.right = '100%';
+    }
+
+    function portfolio() {
+      maincontainer!.style.right = '200%';
+    }
+
+    function profile() {
+      maincontainer!.style.right = '300%';
+    }*/
+
+  //vielleicht irgendwie die url catchen und dann anhand daran und this.page anzeigen was angezeigt wird
+
   render() {
-    return html`
-    <app-header2></app-header2>
-    <app-leaderboard2></app-leaderboard2>`;
+    return html` <app-header></app-header>
+
+      <div id="maincontainer">
+        <div id="news" class="page">news</div>
+        <div id="leaderboard" class="page">leaderboard</div>
+        <div id="portfolio" class="page">portfolio</div>
+        <div id="profile" class="page">profile</div>
+      </div>
+
+      <button id="left" @click="${this._left}">left</button>
+      <button id="right" @click="${this._right}">right</button>`;
+  }
+
+  _left() {
+    const maincontainer = this.shadowRoot!.getElementById('maincontainer');
+
+    if (this.pos == 0) {
+      maincontainer!.style.right = this.pos + '%';
+    } else {
+      this.pos = this.pos - 100;
+      this.page--;
+      maincontainer!.style.right = this.pos + '%';
+      window.history.pushState('obj', 'newtitle', this.pages[this.page]);
+    }
+  }
+
+  _right() {
+    const maincontainer = this.shadowRoot!.getElementById('maincontainer');
+
+    if (this.pos == 300) {
+      maincontainer!.style.right = this.pos + '%';
+    } else {
+      this.pos = this.pos + 100;
+      this.page++;
+      maincontainer!.style.right = this.pos + '%';
+      window.history.pushState('obj', 'newtitle', this.pages[this.page]);
+    }
   }
 }
-
-
