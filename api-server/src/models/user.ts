ba7adacd5be591ab22e4c3070f1c7c48c1ed91be
Nico Pareigis <nico@pareigis.de>
2023-05-12 13:32:09 +0200

    feat: avatar field for user entity

--- api-server/src/models/user.ts
+++ api-server/src/models/user.ts
@@ -1,9 +1,11 @@
 /* Author: Nico Pareigis */
 
+import { Binary } from 'mongodb';
 import { Entity } from './entity';
 
 export interface User extends Entity {
   name: string;
   email: string;
   password: string;
+  avatar: Binary;
 }
--- api-server/src/routes/user.ts
+++ api-server/src/routes/user.ts
@@ -10,11 +10,10 @@ const router = express.Router();
 router.get('/profile', authService.authenticationMiddleware, async (req, res) => {
   const dao: GenericDAO<User> = req.app.locals.userDAO;
 
-  // WARN: security concern?
-  const { name, email, password } = (await dao.findOne(req.app.locals.user))!;
-  const user = { name, email, password };
+  const { avatar, name, email, password } = (await dao.findOne(req.app.locals.user))!;
+  const user = { avatar, name, email, password };
 
-  email ? res.status(200).json(user) : res.status(404).json({ status: 'unable to fetch user information' });
+  email ? res.status(200).json(user) : res.status(404).json({ status: 'failed fetching user information' });
 });
 
 router.post('/profile', authService.authenticationMiddleware, async (req, res) => {
