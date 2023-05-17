ca20d7ddf8347af65e8079cc477352452ff01a77
Nico Pareigis <nico@pareigis.de>
2023-05-17 20:02:04 +0200

    feat: pattern validation

--- api-server/src/routes/user.ts
+++ api-server/src/routes/user.ts
@@ -5,9 +5,7 @@ import { GenericDAO } from '../models/generic.dao.js';
 import { User } from '../models/user.js';
 import { authService } from '../services/auth.service.js';
 
-// TODO:
-// * settle on primary key / identifying property
-// * pattern / content validation
+// TODO: settle on primary key / identifying property
 
 const isValidRequest = <T>(validProperties: string[], obj: T | unknown): obj is T => {
   return Object.getOwnPropertyNames(obj).map(p => validProperties.includes(p)).length == validProperties.length;
@@ -38,9 +36,21 @@ router.post('/profile/avatar', authService.authenticationMiddleware, async (req,
 
 router.post('/profile/details', authService.authenticationMiddleware, async (req, res) => {
   type Details = Pick<User, 'email' | 'name'>;
+
+  // equivalent to internal validation for 'input type="email"' (see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#basic_validation)
+  const reEmail =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const reName = /^[\w-_.]{4,32}$/;
   const dao: GenericDAO<User> = req.app.locals.userDAO;
 
-  if (!isValidRequest<Details>(['email', 'name'], req.body)) return res.status(400).json({ status: 'bad request' });
+  if (
+    !(
+      isValidRequest<Details>(['email', 'name'], req.body) &&
+      reEmail.test(req.body.email) &&
+      reName.test(req.body.name)
+    )
+  )
+    return res.status(400).json({ status: 'bad request' });
 
   dao
     .update(req.body)
@@ -51,8 +61,9 @@ router.post('/profile/details', authService.authenticationMiddleware, async (req
 router.post('/profile/password', authService.authenticationMiddleware, async (req, res) => {
   type Password = Pick<User, 'email' | 'password'>;
   const dao: GenericDAO<User> = req.app.locals.userDAO;
+  const re = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d).{8,32}$/;
 
-  if (!isValidRequest<Password>(['email', 'password'], req.body))
+  if (!(isValidRequest<Password>(['email', 'password'], req.body) && re.test(req.body.password)))
     return res.status(400).json({ status: 'bad request' });
 
   dao
