eb3d9e7aff89073a710d0f76a9bdaefab7d956b8
Nico Pareigis <nico@pareigis.de>
2023-05-17 11:37:42 +0200

    feat!: route changes, request body type validation

--- api-server/src/routes/user.ts
+++ api-server/src/routes/user.ts
@@ -5,6 +5,14 @@ import { GenericDAO } from '../models/generic.dao.js';
 import { User } from '../models/user.js';
 import { authService } from '../services/auth.service.js';
 
+// TODO:
+// * settle on primary key / identifying property
+// * pattern / content validation
+
+const isValidRequest = <T>(validProperties: string[], obj: T | unknown): obj is T => {
+  return Object.getOwnPropertyNames(obj).map(p => validProperties.includes(p)).length == validProperties.length;
+};
+
 const router = express.Router();
 
 router.get('/profile', authService.authenticationMiddleware, async (req, res) => {
@@ -16,21 +24,36 @@ router.get('/profile', authService.authenticationMiddleware, async (req, res) =>
   email ? res.status(200).json(user) : res.status(404).json({ status: 'failed fetching user information' });
 });
 
-router.post('/profile/password', authService.authenticationMiddleware, async (req, res) => {
+router.post('/profile/avatar', authService.authenticationMiddleware, async (req, res) => {
+  type Avatar = Pick<User, 'avatar' | 'email'>;
   const dao: GenericDAO<User> = req.app.locals.userDAO;
 
-  // TODO: refactor to const string[]?
-  type Pass = {
-    email: string;
-    password: string;
-  };
+  if (!isValidRequest<Avatar>(['avatar', 'email'], req.body)) return res.status(400).json({ status: 'bad request' });
 
-  function isPass(obj: Pass): obj is Pass {
-    const props = ['email', 'password'];
-    return Object.getOwnPropertyNames(obj).map(n => props.includes(n)).length == props.length;
-  }
+  dao
+    .update(req.body)
+    .then(() => res.status(200).json({ status: 'ok' }))
+    .catch(() => res.status(500).json({ status: 'error' }));
+});
 
-  if (!isPass(req.body)) return res.status(400).json({ status: 'bad request' });
+router.post('/profile/details', authService.authenticationMiddleware, async (req, res) => {
+  type Details = Pick<User, 'email' | 'name'>;
+  const dao: GenericDAO<User> = req.app.locals.userDAO;
+
+  if (!isValidRequest<Details>(['email', 'name'], req.body)) return res.status(400).json({ status: 'bad request' });
+
+  dao
+    .update(req.body)
+    .then(() => res.status(200).json({ status: 'ok' }))
+    .catch(() => res.status(500).json({ status: 'error' }));
+});
+
+router.post('/profile/password', authService.authenticationMiddleware, async (req, res) => {
+  type Password = Pick<User, 'email' | 'password'>;
+  const dao: GenericDAO<User> = req.app.locals.userDAO;
+
+  if (!isValidRequest<Password>(['email', 'password'], req.body))
+    return res.status(400).json({ status: 'bad request' });
 
   dao
     .update(req.body)
